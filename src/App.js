e5cabf057e9c33286fa8438ddcfd7390c31699b4
malharteli <malhar.teli@gmail.com>
2019-05-03 10:54:09 -0400

    priceline implemented into auctionhouse

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import fire from "./fire";
 import "./App.css";
+import { ThemeProvider, Card, Heading } from "pcln-design-system";
 
 // function Auctions(){
 //   this.dialogs = {}
@@ -20,42 +21,45 @@ class App extends React.Component {
   }
   componentWillMount() {
     let auctionsQuery = fire.firestore().collection("auctions");
-    auctionsQuery.doc("H8ud54fFftYOdZWdgD2v").onSnapshot((doc)=>{
-      console.log('Current data', doc.data())
-    })
+    auctionsQuery.doc("H8ud54fFftYOdZWdgD2v").onSnapshot(doc => {
+      console.log("Current data", doc.data());
+    });
     // auctionsRef("child_added", snapshot => {
     //   let auction = { userId: snapshot.val(), id: snapshot.key };
     //   this.setState({ auctions: [auction].concat(this.state.auctions) });
     // });
   }
 
-
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <code>Hello World!</code>
-          </p>
-          <ul>
-            {
-              this.state.auctions.map(auction => <li key={auction.id}>
-                {auction.userId}
-              </li>)
-            }
-          </ul>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              <code>Hello World!</code>
+            </p>
+            <ul>
+              {this.state.auctions.map(auction => (
+                <li key={auction.id}>{auction.userId}</li>
+              ))}
+            </ul>
+            <Card p="3">
+              <Heading>
+                Card
+              </Heading>
+            </Card>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ThemeProvider>
     );
   }
 }
