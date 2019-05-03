67c4c2e62738e7d31dcc1ccd9d3c5f158e7b5903
malharteli <malhar.teli@gmail.com>
2019-05-03 15:04:15 -0400

    sign in/sign out added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
-import fire from "./fire";
+//Import the Firebase App as configured for our setup
+import fireApp from "./fire";
 import "./App.css";
 import { ThemeProvider, Card, Heading } from "pcln-design-system";
+
+//Import the ACTUAL firebase library
+import firebase from 'firebase'
+//Import the Firebase Authorization
+import  'firebase/auth'
+//Import the Firebase UI package
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 
 // function Auctions(){
 //   this.dialogs = {}
@@ -13,21 +21,38 @@ import { ThemeProvider, Card, Heading } from "pcln-design-system";
 // }
 
 class App extends React.Component {
+  //uiConfig - this is a set of configuration tools that will be used by the React-FirebaseUI module
+  uiConfig = {
+    signInFlow:'popup',
+    signInOptions:[
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      auctions: []
+      auctions: [],
+      isSignedIn: undefined,
     };
   }
   componentWillMount() {
-    let auctionsQuery = fire.firestore().collection("auctions");
+    let auctionsQuery = fireApp.firestore().collection("auctions");
     auctionsQuery.doc("H8ud54fFftYOdZWdgD2v").onSnapshot(doc => {
       console.log("Current data", doc.data());
+    this.unregisterAuthObserver = fireApp.auth().onAuthStateChanged((user)=>{
+      this.setState({isSignedIn: !!user})
+    })
     });
     // auctionsRef("child_added", snapshot => {
     //   let auction = { userId: snapshot.val(), id: snapshot.key };
     //   this.setState({ auctions: [auction].concat(this.state.auctions) });
     // });
+  }
+
+  componentWillUnmount(){
+    this.unregisterAuthObserver();
   }
 
   render() {
@@ -44,6 +69,18 @@ class App extends React.Component {
                 <li key={auction.id}>{auction.userId}</li>
               ))}
             </ul>
+            {this.state.isSignedIn!==undefined && !this.state.isSignedIn &&
+              <div>
+                <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={fireApp.auth()}/>
+              </div>
+
+            }
+            {this.state.isSignedIn &&
+              <div>
+                Hello {fireApp.auth().currentUser.displayName}. You are now signed In!
+                <a type="button" onClick={()=> fireApp.auth().signOut()}>Sign Out</a>
+              </div>
+            }
             <Card p="3">
               <Heading>
                 Card
