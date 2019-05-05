a87e889376268093430ea112ede9bde1d28d31eb
markgreenquist <markgreenquist@gmail.com>
2019-05-05 16:46:01 -0400

    adjusted formField to move it to right margin

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -3,14 +3,16 @@ import { Flex, Text, Icon, FormField, Input } from 'pcln-design-system';
 
 export default function Navbar() {
   return (
-    <Flex color="white" bg="blue" p={2} alignItems="center">
+    <Flex color="black" bg="lightBlue" p={2} alignItems="center">
       <Text bold mx={4}>
         Grab That Spot
       </Text>
-      <FormField ml="auto" mr={4}>
-        <Icon name="Parking" size="20" />
-        <Input id="location" name="location" placeholder="location" />
-      </FormField>
+      <Text ml="auto" mr={4}>
+        <FormField>
+          <Icon name="Parking" size="20" />
+          <Input id="location" name="location" placeholder="location" />
+        </FormField>
+      </Text>
     </Flex>
   );
 }
