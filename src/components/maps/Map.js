57c79f30eeba24af88da9d5872571efbc1c97708
markgreenquist <markgreenquist@gmail.com>
2019-05-06 17:38:34 -0400

    added geoCircle around user current location

--- src/components/maps/Map.js
+++ src/components/maps/Map.js
@@ -64,7 +64,7 @@ export class CurrentLocation extends React.Component {
         {},
         {
           center: center,
-          zoom: 16
+          zoom: zoom
         }
       );
       // maps.Map() is constructor that instantiates the map
@@ -82,6 +82,17 @@ export class CurrentLocation extends React.Component {
     if (map) {
       let center = new maps.LatLng(current.lat, current.lng);
       map.panTo(center);
+
+      let geoCircle = new maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: this.map,
+        center: center,
+        radius: 300
+      });
     }
   }
 
