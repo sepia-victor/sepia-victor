073cb03a345c24d38b5bf8aa1c06dffcd48dfe6d
malharteli <malhar.teli@gmail.com>
2019-05-07 10:52:49 -0400

    prep for merge

--- src/components/AuctionPage.js
+++ src/components/AuctionPage.js
@@ -4,5 +4,16 @@ import fireApp from "../fire";
 
 import { getSingleAuctionData, addAuction }  from '../scripts/Auctions.Data'
 
+export default class AuctionPage extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+        id: "",
+        auction: {}
+      }
+  }
 
+  async componentDidMount(){
 
+  }
+}
