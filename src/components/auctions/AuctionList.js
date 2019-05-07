26f66e4fe7ff66e39692b59667d0343cb273a44f
senor9k <johnvasiliadis@me.com>
2019-05-07 14:37:22 -0400

    auctions list layout

--- src/components/auctions/AuctionList.js
+++ src/components/auctions/AuctionList.js
@@ -2,13 +2,22 @@ import React, { Component } from "react";
 import { error } from "util";
 
 import fireApp from "../../fire";
-import { Card, Heading } from "pcln-design-system";
+import { Card, Heading, Icon, Box, Text, Flex } from "pcln-design-system";
+
 import firebase from "firebase";
 
-import { getAuctionsData, addAuction, getSingleAuctionData }  from '../../scripts/Auctions.Data'
-import { getBidsData, addBidData, highestBidData } from '../../scripts/Bids.Data'
+import {
+  getAuctionsData,
+  addAuction,
+  getSingleAuctionData
+} from "../../scripts/Auctions.Data";
+import {
+  getBidsData,
+  addBidData,
+  highestBidData
+} from "../../scripts/Bids.Data";
 
-import AuctionPage from "./AuctionPage"
+import AuctionPage from "./AuctionPage";
 
 export default class AuctionList extends Component {
   constructor() {
@@ -38,7 +47,7 @@ export default class AuctionList extends Component {
   //     }
   // }
 
-  async componentDidUpdate(){
+  async componentDidUpdate() {
     // console.log(this.getDataArray())
     // this.setState({
     //   name: 'Bar',
@@ -47,7 +56,7 @@ export default class AuctionList extends Component {
   }
 
   async componentDidMount() {
-    let holdArr = [1,3,4];
+    let holdArr = [1, 3, 4];
     let auctionsQuery = fireApp.firestore().collection("auctions");
     // auctionsQuery.get().then(snapshot => { snapshot.forEach(doc => console.log(doc.data()))});
     // try {
@@ -71,14 +80,14 @@ export default class AuctionList extends Component {
       // auctions: await this.getDataArray()
       auctions: await getAuctionsData()
     });
-    console.log(this.state)
+    console.log("-->   ", this.state);
 
     // await addAuction();
     // let bidData= await getBidsData('H8ud54fFftYOdZWdgD2v')
     // let addBid = await addBidData('H8ud54fFftYOdZWdgD2v', 'INAh1ztLO8Y2gpw99MNv0TM2BRV2', 40)
     // let highBid = await highestBidData('H8ud54fFftYOdZWdgD2v')
-    let testAuction = await getSingleAuctionData('H8ud54fFftYOdZWdgD2v')
-    console.log(testAuction)
+    let testAuction = await getSingleAuctionData("H8ud54fFftYOdZWdgD2v");
+    console.log(testAuction);
 
     // await auctionsQuery.get().then(async snapshot => await snapshot.forEach(doc=> holdArr.push(doc.data()))).then( console.log(holdArr)).catch(err=>console.error(err));
 
@@ -89,13 +98,28 @@ export default class AuctionList extends Component {
 
   render() {
     return (
-    <div>
-      {this.state.auctions.map(auction =>(
-        //<AuctionPage auctionId={auction.id}/>
-        <div>
-          {auction.id}
-        </div>
-      ))}
-    </div>);
+      <div>
+        <Heading m={3}>Auctions</Heading>
+        <hr />
+        <Flex wrap>
+          {this.state.auctions.map(auction => (
+            <Box key={auction.id} p={3} width={[1 / 2, 1 / 3, 1 / 4]}>
+              {/* <Text.span px={2}>{auction.id}</Text.span> */}
+              <Text px={2}>
+                <Icon name="CarCircle" color="black" />
+                {auction.location.city}, {auction.location.state}
+              </Text>
+              <Text px={2}>Minimum Bid: ${auction.minimumBid}</Text>
+              <Text px={2}>Grab Now Bid: ${auction.buyNowBid}</Text>
+            </Box>
+          ))}
+        </Flex>
+      </div>
+    );
   }
+}
+
+{
+  /* <Box p={1} bg="lightBlue" key={auction.id}>
+          </Box> */
 }
