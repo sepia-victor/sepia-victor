3d0b3e1460475ae0dda04f9c8f4d5fc464d8a7f3
malharteli <malhar.teli@gmail.com>
2019-05-10 12:51:07 -0400

    thank whatever merciful heavens that persist its WORKING.

--- src/components/bids/AddBid.js
+++ src/components/bids/AddBid.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
-import { Flex, Box, FormField, Input, Icon, Text } from "pcln-design-system";
-import { highestBidData } from "../../scripts/Bids.Data";
+import {
+  Flex,
+  Box,
+  FormField,
+  Input,
+  Icon,
+  Text,
+  Tooltip,
+  RedButton
+} from "pcln-design-system";
+import { highestBidData, addBidData } from "../../scripts/Bids.Data";
 import { getSingleAuctionData } from "../../scripts/Auctions.Data";
 import fireApp from "../../fire";
 import moment from "moment";
 import countdown from "countdown";
 import ReactMomentCountdown from "react-moment-countdown";
+import { tsTypeQuery } from "@babel/types";
 require("moment-countdown");
 
 moment().format();
@@ -27,10 +37,13 @@ class AddBid extends Component {
       userBid: 0,
       highestCurrBid: {},
       unsub: {},
-      timeleft: 0
+      timeleft: 0,
+      bidFlag: true,
+      errorFlag: false,
+      errorMessage: ""
     };
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   // ComponentDidMount will get current highest bid in the specific auction
@@ -44,7 +57,6 @@ class AddBid extends Component {
       auction: await getSingleAuctionData(this.props.auctionId)
     });
     console.log(this.state);
-
   }
 
   onCollectionUpdate = snapshot => {
@@ -69,17 +81,53 @@ class AddBid extends Component {
   //Receives: event
   //Does: Checks given event, and sets the UserBid based upon that event. If the event is invalid, inform user.
   //Return: setState {userbid}
-  handleChange(event){
+  handleChange(event) {
+    let minBid = this.state.highestCurrBid
+      ? this.state.highestCurrBid.offer
+      : this.state.auction.minimumBid;
     event.preventDefault();
-
+    console.log(event.target.value);
+    if (Number(event.target.value) < minBid) {
+      this.setState({
+        bidFlag: true
+      });
+    } else {
+      this.setState({
+        userBid: Number(event.target.value),
+        bidFlag: false
+      });
+    }
   }
 
   //Handle Submit
   //Receives: event
   //Does: Prevent event's default function, submits the userId, the auctionId, and this state's userBid
   //Return: Submits Document to Firestore, Succcess/Failure
-  handleSubmit(event){
-    event.preventDefault()
+  async handleSubmit(event) {
+    let minBid = this.state.highestCurrBid
+      ? this.state.highestCurrBid.offer
+      : this.state.auction.minimumBid;
+    event.preventDefault();
+    console.log("handling the submit");
+    console.log("google currentUser ", fireApp.auth().currentUser.uid);
+    if (fireApp.auth().currentUser) {
+      let auctionId = this.props.auctionId;
+      let userId = fireApp.auth().currentUser.uid;
+      let userBid = this.state.userBid;
+      try {
+        if (userBid > minBid) {
+          let success = await addBidData(auctionId, userId, userBid);
+          console.log(success);
+        } else {
+          this.setState({
+            message: "Needs a higher bid",
+            errorFlag: true
+          });
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 
   render() {
@@ -117,8 +165,20 @@ class AddBid extends Component {
               color="text"
               fontSize={1}
               placeholder="Place Your Bid Here"
+              onChange={this.handleChange}
+              type="number"
             />
           </FormField>
+          {this.state.bidFlag ? (
+            <Tooltip bottom right color="white" bg="red">
+              There is a highet bid.
+            </Tooltip>
+          ) : null}
+          <div>
+            <RedButton size="medium" mr={2} onClick={this.handleSubmit}>
+              Submit
+            </RedButton>
+          </div>
         </Box>
       </Flex>
     );
