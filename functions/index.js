87f731b52f4be1b98de58f09513cd0a5a75a24aa
markgreenquist <markgreenquist@gmail.com>
2019-05-14 12:23:16 -0400

    git requires this apparently

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const functions = require('firebase-functions');
 const twilio = require('twilio');
 const admin = require('firebase-admin');
 
+admin.initializeApp();
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -54,3 +56,56 @@ exports.textStatus = functions.database
 function validE164(num) {
   return /^\+?[1-9]\d{1,14}$/.test(num);
 }
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+// exports.scheduledFunctionCrontab = functions.pubsub.schedule('0 * * * *').onRun((context) => {
+//     console.log('This will be run every day at 1:00 PM UTC!');
+// });
+
+// exports.scheduleTest = functions.pubsub.schedule('0 * * * *').onRun((context)=>{
+//   console.log('Hi!')
+
+//   return admin.firestore().collection('auctions').doc('ASYmSzFOlg6xhv2HfsMr').update({
+//     live: false
+//   })
+// })
+
+// exports.scheduleTestB = functions.pubsub.schedule('30 * * * *').onRun(async (context)=>{
+//   console.log('Bye!')
+//   return await admin.firestore().collection('auctions').doc('ASYmSzFOlg6xhv2HfsMr').update({
+//     live: true
+//   })
+// })
+
+//LiveCheck
+//Receives: Cron trigger
+//Does: Upon triggering, this function will run through every auction and check if the current time is greater than the enddate of the auction. If true, it updates the auction with a false status to live
+//Returns: Nothing of note
+exports.liveCheck = functions.pubsub
+  .schedule('0 * * * *')
+  .onRun(async context => {
+    let auctions = await admin
+      .firestore()
+      .collection('auctions')
+      .get();
+    let response = auctions;
+    let currSecs = (Date.now() / 1000) | 0;
+    auctions.forEach(async auction => {
+      console.log("I'm still here.");
+      let auctionData = await auction.data();
+      if (currSecs > auctionData.auctionEndDate.seconds) {
+        console.log('Auction time vs', currSecs);
+        response = await admin
+          .firestore()
+          .collection('auctions')
+          .doc(auction.id)
+          .update({
+            live: false
+          });
+      } else response = {};
+    });
+    return response;
+  });
